f13bf1e6fb374708db168fce43e1e9ed3d950119
AbhijeetAher <abhijeet.aher23@gmail.com>
2017-11-24 12:27:34 +0530

    Update clamAV_scan_directory.sh

--- clamAV_scan_directory.sh
+++ clamAV_scan_directory.sh
@@ -4,11 +4,9 @@
 
 #SCAN_DIR1="/log"
 SCAN_DIR2="/var/log"
-#SCAN_DIR3="/home/hdfc.admin"
 SCAN_DIR4="/tmp"
 LOG_FILE="/var/log/clamav/manual_clamscan.log"
 #/usr/bin/clamscan -i -r $SCAN_DIR1 >> $LOG_FILE
 /usr/bin/clamscan -i -r $SCAN_DIR2 >> $LOG_FILE
-#/usr/bin/clamscan -i -r $SCAN_DIR3 >> $LOG_FILE
 /usr/bin/clamscan -i -r $SCAN_DIR4 >> $LOG_FILE
 
